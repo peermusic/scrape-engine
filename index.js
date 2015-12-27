0c1db9c6fab90bd187e3b657d94f36a6a20822f5
queicherius <queicherius@gmx.de>
2015-12-27 20:11:51 +0100

    Extract into own functions, fix conditions, fix query parameter generation, change request of title url to not include the album (#2)

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function ScrapeEngine () {
   // $(selection).find('ol.grid-items .grid-items-item a.link-block-target')
   this.filter = [
     '.col-main  ol.grid-items .grid-items-item a.link-block-target ', // Target: Artist 2step
-    '.col-main h2:contains(\'Album\')', // Target: Album 2step
+    ".col-main h2:contains('Album')", // Target: Album 2step
     '.chartlist .chartlist-name a.link-block-target', // Target: Titles
     '.col-main .grid-items-section .grid-items-item-main-text a.link-block-target', // Similar Artist 2Step
     '.col-main .grid-items a.link-block-target', // Similar Artist over "+similar" URL
@@ -179,6 +179,12 @@ ScrapeEngine.prototype.getURLArtist = function (artist, callback) {
 // Get the metadata of title
 ScrapeEngine.prototype.getMetadata = function (list, result, callback) {
   var self = this
+
+  // Nothing found
+  if (list.length === 0) {
+    return callback(null, result)
+  }
+
   var url = self.lastfmURL + list[result.length].href
   console.log('URL: ' + url)
   // Get the page of the title
@@ -208,15 +214,82 @@ ScrapeEngine.prototype.getMetadata = function (list, result, callback) {
 
 // Get a list of object, which contains similar title information and the url
 ScrapeEngine.prototype.getSimilarTitle = function (title, album, artist, genre, callback) {
-  var self = this
-  var result = []
-  var url = ''
+  // Cleanup request
+  title = title === '' || !title ? null : title
+  album = album === '' || !album ? null : album
+  artist = artist === '' || !artist ? null : artist
+  genre = genre === '' || !genre ? null : genre
+  console.log('Getting similar title for...', {title: title, album: album, artist: artist, genre: genre})
+
+  // Get similar title by title, artist and album
+  if (title && album && artist) {
+    return this.getSimilarTitleByTitle(title, album, artist, callback)
+  }
+
+  // Get similar title by artist and album
+  if (album && artist) {
+    return this.getSimilarTitleByAlbum(album, artist, callback)
+  }
+
+  // Get similar title by artist
+  if (artist) {
+    return this.getSimilarTitleByArtist(artist, callback)
+  }
 
   // Get similar title by genre
-  if (genre !== undefined || genre !== '' || title === undefined || title === '' || album === '' || artist === '') {
-    // Get the tag page
-    url = self.lastfmURL + '/tag/' + encodeURI(genre)
-    console.log(url)
+  if (genre) {
+    return this.getSimilarTitleByGenre(genre, callback)
+  }
+
+  return callback('No valid request data found')
+}
+
+ScrapeEngine.prototype.getSimilarTitleByGenre = function (genre, callback) {
+  var self = this
+
+  // Get the tag page
+  var url = self.lastfmURL + '/tag/' + encodeURI(genre)
+  console.log('Getting similar title by genre: ' + url)
+  got(url, function (err, html) {
+    if (err) {
+      callback(err, html)
+      return
+    }
+
+    var $ = cheerio.load(html)
+    // Error 404: If Page not found
+    if ($('.page-content h1').text() === '404 - Page Not Found') {
+      callback('Error: 404', html)
+      return
+    }
+
+    // Push the links of the similar title into list
+    var list = $(self.filter[9]).map(function (i, el) {
+      el = $(el)
+      var row = {
+        href: el.attr('href'),
+        content: el.text()
+      }
+      console.log(row)
+      return row
+    }).get()
+    self.getMetadata(list, [], callback)
+  })
+}
+
+ScrapeEngine.prototype.getSimilarTitleByArtist = function (artist, callback) {
+  var self = this
+
+  // Get the URL of artist
+  this.getURLArtist(artist, function (err, artistURL) {
+    if (err) {
+      callback(err, artistURL)
+      return
+    }
+
+    var url = self.lastfmURL + artistURL
+    console.log('Getting similar title by artist: ' + url)
+    // Get the artist page
     got(url, function (err, html) {
       if (err) {
         callback(err, html)
@@ -224,13 +297,7 @@ ScrapeEngine.prototype.getSimilarTitle = function (title, album, artist, genre, 
       }
 
       var $ = cheerio.load(html)
-      // Error 404: If Page not found
-      try {
-        if ($('.page-content h1').text() === '404 - Page Not Found') throw 'Error: 404'
-      } catch (err) {
-        callback(err, html)
-      }
-      // Push the links of the similar title into list
+      // Push the links of the top title into list
       var list = $(self.filter[9]).map(function (i, el) {
         el = $(el)
         var row = {
@@ -240,32 +307,16 @@ ScrapeEngine.prototype.getSimilarTitle = function (title, album, artist, genre, 
         console.log(row)
         return row
       }).get()
-      self.getMetadata(list, result, callback)
-    })
-    return
-  }
-
-  // Get similar title by artist
-  if (title === undefined || title === '' || album === '' || artist !== '') {
-    // Get the URL of artist
-    this.getURLArtist(artist, function (err, artistURL) {
-      if (err) {
-        callback(err, artistURL)
-        return
-      }
-
-      url = self.lastfmURL + artistURL
-      console.log(url)
-      // Get the artist page
+      // Take the first entry in list as target
+      url = self.lastfmURL + list[0].href
+      // Get the page of the specified title
       got(url, function (err, html) {
         if (err) {
-          callback(err, html)
-          return
+          return callback(err, html)
         }
-
         var $ = cheerio.load(html)
-        // Push the links of the top title into list
-        var list = $(self.filter[9]).map(function (i, el) {
+        // Push similar title of the given title into list
+        var list = $(self.filter[5]).map(function (i, el) {
           el = $(el)
           var row = {
             href: el.attr('href'),
@@ -274,41 +325,43 @@ ScrapeEngine.prototype.getSimilarTitle = function (title, album, artist, genre, 
           console.log(row)
           return row
         }).get()
-        // Take the first entry in list as target
-        url = self.lastfmURL + list[0].href
-        // Get the page of the specified title
-        got(url, function (err, html) {
-          if (err) {
-            callback(err, html)
-            return
-          }
-          var $ = cheerio.load(html)
-          // Push similar title of the given title into list
-          var list = $(self.filter[5]).map(function (i, el) {
-            el = $(el)
-            var row = {
-              href: el.attr('href'),
-              content: el.text()
-            }
-            console.log(row)
-            return row
-          }).get()
-          self.getMetadata(list, result, callback)
-        })
+        self.getMetadata(list, [], callback)
       })
     })
-    return
-  }
+  })
+}
 
-  // Get similar title by artist and album
-  if (title === undefined || title === '' || album !== '' || artist !== '') {
-    this.getURLAlbum(album, artist, function (err, albumURL) {
+ScrapeEngine.prototype.getSimilarTitleByAlbum = function (album, artist, callback) {
+  var self = this
+
+  this.getURLAlbum(album, artist, function (err, albumURL) {
+    if (err) {
+      callback(err, albumURL)
+      return
+    }
+    var url = self.lastfmURL + albumURL
+    console.log('Getting similar title by album: ' + url)
+    // Get the page of the album
+    got(url, function (err, html) {
       if (err) {
-        callback(err, albumURL)
+        callback(err, html)
         return
       }
-      url = self.lastfmURL + albumURL
-      // Get the page of the album
+
+      var $ = cheerio.load(html)
+      // Push the titles of the album into list
+      var list = $(self.filter[2]).map(function (i, el) {
+        el = $(el)
+        var row = {
+          href: el.attr('href'),
+          content: el.text()
+        }
+        console.log(row)
+        return row
+      }).get()
+      // Take the first entry in list as target
+      url = self.lastfmURL + list[0].href
+      // Get the page of the specified title
       got(url, function (err, html) {
         if (err) {
           callback(err, html)
@@ -316,8 +369,8 @@ ScrapeEngine.prototype.getSimilarTitle = function (title, album, artist, genre, 
         }
 
         var $ = cheerio.load(html)
-        // Push the titles of the album into list
-        var list = $(self.filter[2]).map(function (i, el) {
+        // Push similar title of the given title into list
+        var list = $(self.filter[5]).map(function (i, el) {
           el = $(el)
           var row = {
             href: el.attr('href'),
@@ -326,41 +379,24 @@ ScrapeEngine.prototype.getSimilarTitle = function (title, album, artist, genre, 
           console.log(row)
           return row
         }).get()
-        // Take the first entry in list as target
-        url = self.lastfmURL + list[0].href
-        // Get the page of the specified title
-        got(url, function (err, html) {
-          if (err) {
-            callback(err, html)
-            return
-          }
-
-          var $ = cheerio.load(html)
-          // Push similar title of the given title into list
-          var list = $(self.filter[5]).map(function (i, el) {
-            el = $(el)
-            var row = {
-              href: el.attr('href'),
-              content: el.text()
-            }
-            console.log(row)
-            return row
-          }).get()
-          self.getMetadata(list, result, callback)
-        })
+        self.getMetadata(list, [], callback)
       })
     })
-    return
-  }
+  })
+}
 
-  // Get similar title by title, artist and album
-  this.getURLTitle(title, album, artist, function (err, titleURL) {
+ScrapeEngine.prototype.getSimilarTitleByTitle = function (title, album, artist, callback) {
+  var self = this
+
+  this.getURLTitle(title, artist, function (err, titleURL) {
     if (err) {
       callback(err, titleURL)
       return
     }
 
     var url = self.lastfmURL + titleURL
+    console.log('Getting similar title by title: ' + url)
+
     // Get the page of the title
     got(url, function (err, html) {
       if (err) {
@@ -379,17 +415,18 @@ ScrapeEngine.prototype.getSimilarTitle = function (title, album, artist, genre, 
         console.log(row)
         return row
       }).get()
-      self.getMetadata(list, result, callback)
+      console.log('Contents', list)
+      self.getMetadata(list, [], callback)
     })
   })
 }
 
 // Get the url of a specific title
-ScrapeEngine.prototype.getURLTitle = function (title, album, artist, callback) {
+ScrapeEngine.prototype.getURLTitle = function (title, artist, callback) {
   var self = this
   var list = []
   // Get the search page with the specified data
-  got(this.createQueryURL(artist, album, title), function (err, html) {
+  got(this.createQueryURL(artist, title), function (err, html) {
     if (err) {
       callback(err, html)
       return
@@ -412,12 +449,17 @@ ScrapeEngine.prototype.getURLTitle = function (title, album, artist, callback) {
 
 // Creates an query url with the passed metadata
 ScrapeEngine.prototype.createQueryURL = function (artist, album, title) {
-  var result = this.lastfmURL + '/search?q='
-  for (var i = 0; i < arguments.length; i++) {
-    if (arguments[i]) {
-      result += encodeURI(arguments[i])
-    }
-  }
-  console.log(result)
+  var query = _objectValues(arguments).map(function (x) { return encodeURI(x) })
+  var result = this.lastfmURL + '/search?q=' + query.join('%20')
+  console.log('Query url', result)
   return result
+}
+
+// Get the object values as an array
+function _objectValues (object) {
+  var array = []
+  for (var i in object) {
+    array.push(object[i])
+  }
+  return array
 }
